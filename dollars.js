06658405e3e78e99ed0a901894e49ec3bcf50285
David Verhasselt <david@crowdway.com>
2012-01-19 22:03:06 +0200

    Add filterKeys, make it work.

--- dollars.js
+++ dollars.js
@@ -1,39 +1,40 @@
-$$ = {
-  // Formatting Methods
-  var formatSize = function(size, precisionArg, thresholdArg) {
-    var units = " bytes, KB, MB, GB".split(",");
-    var precision = precisionArg || 1;
-    var threshold = thresholdArg || 500;
-    var kilo = 1024;
+$$ = dollars = {};
 
-    for (var dim = 0; dim < units.length-1; dim++)
-    if (size < thresholdArg) {
-      return size.toFixed(precision) + units[dim];
+// Formatting Methods
+dollars.formatSize = function(size, precisionArg, thresholdArg) {
+  var units = " bytes, KB, MB, GB".split(",");
+  var precision = precisionArg || 1;
+  var threshold = thresholdArg || 500;
+  var kilo = 1024;
+
+  for (var dim = 0; dim < units.length-1; dim++) {
+    if (size < threshold) {
+      return (size % 1 ? size.toFixed(precision) : size) + units[dim];
     } else {
       size = size / kilo;
     }
-
-    return size.toFixed(precision) + units.pop();
   }
 
-  // Creates a copy from `collection` with only the key-value
-  // pairs specified by `keys`.
-  //
-  // Example:
-  //
-  //  var obj = {name: "David", age: 25, email: "david@crowdway.com"};
-  //  $$.filterKeys(obj, ["name", "age"]);
-  //  // This would return
-  //  { name: "David", age: 25 }
-  var filterKeys = function(collection, keys) {
-    var results = {};
+  return size.toFixed(precision) + units.pop();
+}
 
-    _.each(collection, function(value, key) {
-      if (_.include(keys, key)) {
-        results[key] = value;
-      }
-    });
-    
-    return results;
-  }
+// Creates a copy from `collection` with only the key-value
+// pairs specified by `keys`.
+//
+// Example:
+//
+//  var obj = {name: "David", age: 25, email: "david@crowdway.com"};
+//  $$.filterKeys(obj, ["name", "age"]);
+//  // This would return
+//  { name: "David", age: 25 }
+dollars.filterKeys = function(collection, keys) {
+  var results = {};
+
+  _.each(collection, function(value, key) {
+    if (_.include(keys, key)) {
+      results[key] = value;
+    }
+  });
+  
+  return results;
 }

b2f25552a3b7fc7c7aa1b35080bce69015d8dc5d
David Verhasselt <david@crowdway.com>
2012-01-19 21:28:43 +0200

    Add filterKeys method

--- dollars.js
+++ dollars.js
@@ -1,5 +1,6 @@
 $$ = {
-  var  formatSize = function(size, precisionArg, thresholdArg) {
+  // Formatting Methods
+  var formatSize = function(size, precisionArg, thresholdArg) {
     var units = " bytes, KB, MB, GB".split(",");
     var precision = precisionArg || 1;
     var threshold = thresholdArg || 500;
@@ -13,5 +14,26 @@ $$ = {
     }
 
     return size.toFixed(precision) + units.pop();
+  }
+
+  // Creates a copy from `collection` with only the key-value
+  // pairs specified by `keys`.
+  //
+  // Example:
+  //
+  //  var obj = {name: "David", age: 25, email: "david@crowdway.com"};
+  //  $$.filterKeys(obj, ["name", "age"]);
+  //  // This would return
+  //  { name: "David", age: 25 }
+  var filterKeys = function(collection, keys) {
+    var results = {};
+
+    _.each(collection, function(value, key) {
+      if (_.include(keys, key)) {
+        results[key] = value;
+      }
+    });
+    
+    return results;
   }
 }
